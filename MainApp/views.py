5c8da0aa1c2d92e500a2c49f561ce0dd792056bb
dsolovov <dsolovov@inbox.ru>
2024-03-01 01:42:19 +0300

    task1-2

---
+++ MainApp/views.py
@@ -0,0 +1,38 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+author = {
+    "Имя": "Иван",
+    "Отчество": "Петрович",
+    "Фамилия": "Иванов",
+    "телефон": "8-923-600-01-02",
+    "email": "vasya@mail.ru",
+}
+
+items = [
+   {"id": 1, "name": "Кроссовки abibas", "quantity": 5},
+   {"id": 2, "name": "Куртка кожаная", "quantity": 2},
+   {"id": 5, "name": "Coca-cola 1 литр", "quantity": 12},
+   {"id": 7, "name": "Картофель фри", "quantity": 0},
+   {"id": 8, "name": "Кепка", "quantity": 124},
+]
+
+
+def home(request):
+    text = """<h1>"Изучаем django"</h1>
+        <strong>Автор</strong>: <i>Иванов И.П.</i>
+        """
+    return HttpResponse(text)
+
+
+def about(request):
+    text = f"""
+    Имя: <b>{author["Имя"]}</b><br>
+    Отчество: <b>{author["Отчество"]}</b><br>
+    Фамилия: <b>{author["Фамилия"]}</b><br>
+    телефон: <b>{author["телефон"]}</b><br>
+    email: <b>{author["email"]}</b><br>
+    """
+    return HttpResponse(text)
+
